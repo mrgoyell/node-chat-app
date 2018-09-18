d2e90dd0e0bfdec001a0b4aa3de10da15b572a45
coder_goyell <codergoyell@gmail.com>
2018-09-18 20:57:34 +0530

    form for messages and incoming messages in browser

--- public/js/index.js
+++ public/js/index.js
@@ -6,6 +6,8 @@ socket.on('connect', function () {
   // socket.emit('createMessage', {
   //   from: 'Andrew',
   //   text: 'Yup, that works for me.'
+  // }, function(mess){
+  //   console.log('Got it',mess);
   // });
 });
 
@@ -15,4 +17,17 @@ socket.on('disconnect', function () {
 
 socket.on('newMessage', function (message) {
   console.log('newMessage', message);
+  let li = jQuery('<li></li>');
+  li.text(`${message.from}: ${message.text}`);
+  jQuery('#messages').append(li);
+});
+
+jQuery('#message-form').on('submit',function(e){
+  e.preventDefault();
+  socket.emit('createMessage',{
+    from: 'User',
+    text: jQuery('[name=message]').val()
+  }, function(){
+
+  });
 });
