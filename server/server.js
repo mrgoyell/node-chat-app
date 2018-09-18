0bf54acaff6e789d31fe1180dd86461fee41b075
coder_goyell <codergoyell@gmail.com>
2018-09-18 15:43:53 +0530

    connect nd disconnect handlers

--- server/server.js
+++ server/server.js
@@ -1,13 +1,23 @@
 const path = require('path');
 const pubPath = path.join(__dirname,'../public');
 const express = require('express');
+const socketIO = require('socket.io');
+const http = require('http');
 const port = process.env.PORT || 3000;
 let app = express();
+let server = http.createServer(app);
+var io = socketIO(server);
 app.use(express.static(pubPath));
 // app.get('/',()=>{
 //     app.render()
 // })
+io.on('connection',(socket)=>{
+    console.log('New user connected');
+    socket.on('disconnect',()=>{
+        console.log('disconnected from client');
+    });
+});
 
-app.listen(3000,()=>{
-    console.log('Server is up on port 3000');
-})
+server.listen(port,()=>{
+    console.log('Server is up on port'+port);
+});
