3b34747f6e9ee496eac5636380c34d19a10a7b0f
coder_goyell <codergoyell@gmail.com>
2018-09-19 19:51:31 +0530

    chat app styling

--- public/js/index.js
+++ public/js/index.js
@@ -33,11 +33,12 @@ socket.on('newLocationMessage', function(message){
 
 jQuery('#message-form').on('submit',function(e){
   e.preventDefault();
+  let messageTextbox = jQuery('[name=message]');
   socket.emit('createMessage',{
     from: 'User',
-    text: jQuery('[name=message]').val()
+    text: messageTextbox.val()
   }, function(){
-
+      messageTextbox.val('')
   });
 });
 
@@ -46,13 +47,16 @@ locationButton.on('click',function(){
   if(!navigator.geolocation){
     return alert('Geolocation not supported on your browser');
   }
+  locationButton.attr('disabled','disabled').text('Sending location...');
   navigator.geolocation.getCurrentPosition(function(position){
     // console.log(position);
+    locationButton.removeAttr('disabled').text('Send location');
     socket.emit('createLocationMessage',{
       latitude: position.coords.latitude,
       longitude: position.coords.longitude
     });
   },function(){
+    locationButton.removeAttr('disabled').text('Send location');
     alert('Unable to fetch position');
   });
 });
